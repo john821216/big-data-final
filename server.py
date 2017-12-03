7a38befabc26d8fe0c5e6e4a91903655eaddd788
john821216 <john821216@gmail.com>
2017-12-03 17:06:09 -0500

    new commit

--- server.py
+++ server.py
@@ -30,9 +30,41 @@ cron = Scheduler(daemon=True)
 # Explicitly kick off the background thread
 cron.start()
 
+DATABASEURI = "postgresql://localhost/big_data"
+engine = create_engine(DATABASEURI)
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request.
+
+  The variable g is globally accessible.
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print "uh oh, problem connecting to database"
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't, the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+
 
 @app.route('/')
 def index(): 
+  cursor = g.conn.execute("SELECT * FROM account")
+  print cursor
   return render_template('signIn.html')
 
 @app.route('/test')
